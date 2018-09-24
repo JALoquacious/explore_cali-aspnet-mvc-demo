c87ae4178f9194b0baf9ffcc971faf2d829f5766
JALoquacious <jalikoudis@protonmail.com>
2018-09-24 15:20:19 -0400

    Use dependency injection for feature config

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -8,24 +8,33 @@ namespace ExploreCalifornia
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+        private readonly IConfigurationRoot configuration;
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public Startup(IHostingEnvironment env)
         {
-            app.UseExceptionHandler("/error.html");
-
-            var configuration = new ConfigurationBuilder()
+            configuration = new ConfigurationBuilder()
                 .AddEnvironmentVariables()
                 .AddJsonFile(env.ContentRootPath + "/config.json")
                 .AddJsonFile(env.ContentRootPath + "/config.development.json", true)
                 .Build();
+        }
 
-            if (configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions"))
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                EnableDeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, FeatureToggles features)
+        {
+            app.UseExceptionHandler("/error.html");
+
+            if (features.EnableDeveloperExceptions)
             {
                 app.UseDeveloperExceptionPage();
             }
