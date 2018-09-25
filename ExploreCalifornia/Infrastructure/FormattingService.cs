354a0d7351f06ce9cd281e6a4360b1eeeddd605d
JALoquacious <jalikoudis@protonmail.com>
2018-09-25 15:05:04 -0400

    Register and inject FormattingService for blog post dates

---
+++ ExploreCalifornia/Infrastructure/FormattingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExploreCalifornia.Infrastructure
+{
+    public class FormattingService
+    {
+        public string AsReadableDate(DateTime date)
+        {
+            return date.ToString("d");
+        }
+    }
+}
--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using ExploreCalifornia.Infrastructure;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +24,8 @@ namespace ExploreCalifornia
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<FormattingService>();
+
             services.AddTransient<FeatureToggles>(x => new FeatureToggles
             {
                 EnableDeveloperExceptions = configuration
