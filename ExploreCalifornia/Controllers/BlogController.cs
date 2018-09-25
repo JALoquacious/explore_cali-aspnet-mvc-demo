ce1835f8620a80f31df842924baed40f7f78521a
JALoquacious <jalikoudis@protonmail.com>
2018-09-25 14:55:25 -0400

    Convert Blog posts to statically-typed partial views

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 
 namespace ExploreCalifornia.Controllers
@@ -9,18 +10,46 @@ namespace ExploreCalifornia.Controllers
         [Route("")]
         public IActionResult Index()
         {
-            return View();
+            var posts = new[]
+            {
+                new Post
+                {
+                    Title = "Post 1",
+                    Posted = DateTime.Now + TimeSpan.FromHours(1),
+                    Author = "Author 1",
+                    Body = "Content body 1"
+                },
+                new Post
+                {
+                    Title = "Post 2",
+                    Posted = DateTime.Now + TimeSpan.FromHours(2),
+                    Author = "Author 2",
+                    Body = "Content body 2"
+                },
+                new Post
+                {
+                    Title = "Post 3",
+                    Posted = DateTime.Now + TimeSpan.FromHours(3),
+                    Author = "Author 3",
+                    Body = "Content body 3"
+                }
+            };
+
+            return View(posts);
         }
 
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-            ViewBag.Title = "My blog post";
-            ViewBag.Posted = DateTime.Now;
-            ViewBag.Author = "Joe Schmoe";
-            ViewBag.Body = "This is a great blog post, don't you think?";
+            var post = new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "Joe Schmoe",
+                Body = "This is a great blog post, don't you think?"
+            };
 
-            return View();
+            return View(post);
         }
     }
 }
