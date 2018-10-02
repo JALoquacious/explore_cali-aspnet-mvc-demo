dacd546b13fe8905f133eda93d804ba5746d7875
JALoquacious <jalikoudis@protonmail.com>
2018-10-02 11:16:33 -0400

    Add comment functionality to blog API

--- ExploreCalifornia/Models/BlogDataContext.cs
+++ ExploreCalifornia/Models/BlogDataContext.cs
@@ -4,6 +4,7 @@ namespace ExploreCalifornia.Models
 {
     public class BlogDataContext : DbContext
     {
+        public DbSet<Comment> Comments { get; set; }
         public DbSet<Post> Posts { get; set; }
 
         public BlogDataContext(DbContextOptions<BlogDataContext> options)
